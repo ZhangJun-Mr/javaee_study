1b01e1fe88a344fd4de8eb71d989ac2eecc36a38
zhangjun <zhangjun@qiatuchina.com>
2018-08-24 08:51:08 +0800

    push

--- src/main/java/com/service/impl/RedisRedPacketServiceImpl.java
+++ src/main/java/com/service/impl/RedisRedPacketServiceImpl.java
@@ -5,7 +5,6 @@ import com.service.RedisRedPacketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 
 import javax.sql.DataSource;
