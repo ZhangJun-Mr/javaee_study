9de1903b478d28ffb7c61be44f6f20e5616bea83
zhangjun <zhangjun@qiatuchina.com>
2018-08-29 09:21:19 +0800

    push

--- src/main/java/com/controller/MyController.java
+++ src/main/java/com/controller/MyController.java
@@ -1,9 +1,13 @@
 package com.controller;
 
+import com.dao.RedPacketMapper;
+import com.pojo.RedPacket;
+import com.pojo.UserRedPacket;
 import com.service.RedPacketService;
 import com.service.UserRedPacketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +21,9 @@ public class MyController {
     @Autowired
     UserRedPacketService userRedPacketService;
 
+    @Autowired
+    RedPacketMapper redPacketMapper;
+
     @RequestMapping("/grapRedPacket")
     public Map<String, Object> invoke(Long redPacketId, Long userId) {
         int result = userRedPacketService.grapRedPacket(redPacketId, userId);
@@ -25,5 +32,15 @@ public class MyController {
         retMap.put("success", flag);
         retMap.put("message", flag ? "抢红包成功" : "抢红包失败");
         return retMap;
+    }
+
+    @GetMapping("/getAll")
+    public UserRedPacket getAll(Long redPacketId, Long userId) {
+        return redPacketMapper.getAll(userId, redPacketId);
+    }
+
+    @GetMapping("/update")
+    public int update(String note, double amount) {
+        return redPacketMapper.update(note, amount);
     }
 }
--- src/main/java/com/dao/RedPacketMapper.java
+++ src/main/java/com/dao/RedPacketMapper.java
@@ -1,11 +1,13 @@
 package com.dao;
 
 import com.pojo.RedPacket;
+import com.pojo.UserRedPacket;
 import org.apache.ibatis.annotations.Param;
 
 public interface RedPacketMapper {
     /**
      * 获取红包信息
+     *
      * @param id 红包id
      * @return 红包具体信息
      */
@@ -13,9 +15,13 @@ public interface RedPacketMapper {
 
     /**
      * 扣减抢红包数
-     * @param id 红包id
+     *
+     * @param id      红包id
      * @param version 版本号
      * @return 更新记录条数
      */
     int decreaseRedPacket(@Param("id") Long id, @Param("version") Integer version);
+
+    UserRedPacket getAll(@Param("id") Long id, @Param("redPacketId") Long redPacketId);
+    int update(@Param("note") String note, @Param("amount") double amount);
 }
