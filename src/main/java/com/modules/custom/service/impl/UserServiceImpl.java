872511275151d8bc6776e668b4c92089970fa93e
zhangjun <beck.zhang@softtek.com>
2019-03-30 14:42:42 +0800

    commit

--- src/main/java/com/modules/custom/service/impl/UserServiceImpl.java
+++ src/main/java/com/modules/custom/service/impl/UserServiceImpl.java
@@ -22,6 +22,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> findList(User user) {
-        return null;
+        return userMapper.findList(user);
     }
 }
